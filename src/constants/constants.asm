96dc218be45d097601a6d357c719aa94f4fbde5c
danielh05 <daniel_henkel@yahoo.com>
2021-12-27 22:55:06 +0100

    seperate structures

---
+++ src/constants/constants.asm
@@ -0,0 +1,9 @@
+;CONSTANTS
+
+.include "sfx_constants/sfx_constants.asm"
+
+.include "music_constants/music_constants.asm"
+
+.include "enemy_obj_constants/enemy_obj_constants.asm"
+
+.include "other_constants/other_constants.asm"
---
+++ src/constants/other_constants/other_constants.asm
@@ -0,0 +1,35 @@
+;other constants
+World1 = 0
+World2 = 1
+World3 = 2
+World4 = 3
+World5 = 4
+World6 = 5
+World7 = 6
+World8 = 7
+Level1 = 0
+Level2 = 1
+Level3 = 2
+Level4 = 3
+
+WarmBootOffset        = <$07d6
+ColdBootOffset        = <$07fe
+TitleScreenDataOffset = $1ec0
+SoundMemory           = $07b0
+SwimTileRepOffset     = PlayerGraphicsTable + $9e
+MusicHeaderOffsetData = MusicHeaderData - 1
+MHD                   = MusicHeaderData
+
+A_Button              = %10000000
+B_Button              = %01000000
+Select_Button         = %00100000
+Start_Button          = %00010000
+Up_Dir                = %00001000
+Down_Dir              = %00000100
+Left_Dir              = %00000010
+Right_Dir             = %00000001
+
+TitleScreenModeValue  = 0
+GameModeValue         = 1
+VictoryModeValue      = 2
+GameOverModeValue     = 3
